b7ed38a327117a8ef272fa16b206eb1266f3b3d2
hyundong kim <100841549+miffDONG@users.noreply.github.com>
2024-04-12 01:16:16 +0900

    Title: 행복한 수열의 개수, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240412/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240412/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,24 +1,18 @@
-import sys
-sys.stdin=open('input.txt','r')
-
-if __name__=="__main__":
-    n,m=map(int,input().split())
-    li = []
-    happy_count = 0
-    # 첫 row 입력을 받는 for문을 지나치지 않고 활용.
-    for _ in range(n):
-        row = list(map(int,input().split()))
-        li.append(row)
-        for i in range(n-m+1):
-            if row[i:i+m].count(row[i]) == m:
-                happy_count+=1
-                break
-
-    # zip을 통한 
-    for cow in zip(*li):
-        for i in range(n-m+1):
-            if cow[i:i+m].count(cow[i]) == m:
-                happy_count+=1
-                break
-
-    print(happy_count)
+n,m=map(int, input().split())
+board = []
+answer = 0
+for _ in range(n):
+    row = list(map(int, input().split()))
+    board.append(row)
+    for i in range(n-m+1):
+        if row[i:i+m].count(row[i]) == m:
+            answer+=1
+            break
+            
+for cow in zip(*board):
+    for i in range(n-m+1):
+        if cow[i:i+m].count(cow[i]) == m:
+            answer+=1
+            break
+        
+print(answer)
